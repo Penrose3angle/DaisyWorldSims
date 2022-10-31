2f233f21cdf64de5daf437c93343e6c691b036b9
Penrose3angle <fowl_ireland@hotmail.com>
2022-10-31 22:35:48 +0700

    update spawning method

--- agents.py
+++ agents.py
@@ -12,7 +12,25 @@ class Daisy(mesa.Agent):
         self.remaining_lifespan = remaining_lifespan  # remaining lifespan of the daisy (timestep)
 
     def step(self):
-        # Change the local temperature
+        self.change_local_temperature()
+        self.spawn()
+        self.check_lifespan()
+
+    def check_lifespan(self):
+        self.remaining_lifespan -= 1
+        if self.remaining_lifespan < 0:
+            self.model.grid.remove_agent(self)
+            self.model.schedule.remove(self)
+
+    def change_local_temperature(self):
+        """
+        Change the local temperature
+        every patch will calculate the temperature at that spot based on
+        (1) the energy absorbed by the daisy at that patch and
+        (2) the diffusion of 50% of the temperature value at that patch between its neighbors
+        :param grid_content:
+        :return:
+        """
         grid_content = self.model.grid.get_cell_list_contents(self.pos)
         grass = [a for a in grid_content if isinstance(a, GrassPatch)][0]
         absorbed_luminosity = (1 - self.albedo) * self.model.solar_luminosity
@@ -20,54 +38,62 @@ class Daisy(mesa.Agent):
             local_heating = 72 * math.log(absorbed_luminosity) + 80
         else:
             local_heating = 80
-        grass.local_temperature = float((grass.local_temperature + local_heating)/2)
+        grass.local_temperature = float((grass.local_temperature + local_heating) / 2)
 
-        # Random chance of spawning to neighbouring grid
-        seed_threshold = ((0.1457 * self.model.world_temperature) - (0.0032 * (self.model.world_temperature ** 2)) - 0.6443)
-        # Successful spawning
+    def spawn(self):
+        """
+        Random chance of spawning to neighbouring grid based on the local(?) world(?) temperature
+        :param grid_content:
+        :return:
+        """
+        # grid_content = self.model.grid.get_cell_list_contents(self.pos)
+        # grass = [a for a in grid_content if isinstance(a, GrassPatch)][0]
+        # local_temp = grass.local_temperature
+        seed_threshold = (
+                (0.1457 * self.model.world_temperature) - (0.0032 * (self.model.world_temperature ** 2)) - 0.6443)
         seed = random.random()
-        # print(seed, seed_threshold, (seed<seed_threshold))
         if seed < seed_threshold:
             neighbourhood = self.model.grid.get_neighborhood(
                 self.pos,
                 moore=True,
                 include_center=False)
             new_position = self.random.choice(neighbourhood)
-            daisy = Daisy(
-                self.model.next_id(), new_position, self.model, self.albedo, 5
-            )
-            self.model.grid.place_agent(daisy, new_position)
-            self.model.schedule.add(daisy)
+            grid_content = self.model.grid.get_cell_list_contents(new_position)
+            daisies = [a for a in grid_content if isinstance(a, GrassPatch) is False]
+            if len(daisies) == 0:
+                daisy = self._create_offspring(new_position)
+                self.model.grid.place_agent(daisy, new_position)
+                self.model.schedule.add(daisy)
 
-        # Use up one timestep
-        self.remaining_lifespan -= 1
-
-        # Die of old age - don't put this part first otherwise agent will die but the rest of the code still try to run.
-        if self.remaining_lifespan < 0:
-            self.model.grid.remove_agent(self)
-            self.model.schedule.remove(self)
+    def _create_offspring(self, new_position):
+        raise NotImplementedError
 
 
 class WhiteDaisy(Daisy):
-    pass
+    def _create_offspring(self, new_position):
+        daisy = WhiteDaisy(self.model.next_id(), new_position, self.model, self.albedo, 5)
+        return daisy
 
 
 class BlackDaisy(Daisy):
-    pass
+    def _create_offspring(self, new_position):
+        daisy = BlackDaisy(self.model.next_id(), new_position, self.model, self.albedo, 5)
+        return daisy
 
 
 class GrassPatch(mesa.Agent):
-    """
-    A patch of grass that grows at a fixed rate and it is eaten by sheep
-    """
-
-    def __init__(self, unique_id, pos, model, local_temperature):
+    def __init__(self, unique_id, pos, model, local_temperature, albedo):
         """
         Creates a new patch of grass
         Args:
-            grown: (boolean) Whether the patch of grass is fully grown or not
-            countdown: Time for the patch of grass to be fully grown again
         """
         super().__init__(unique_id, model)
         self.local_temperature = local_temperature
         self.pos = pos
+        self.albedo = albedo
+
+    def step(self):
+        grid_content = self.model.grid.get_cell_list_contents(self.pos)
+        daisies = [a for a in grid_content if isinstance(a, GrassPatch) is False]
+        if len(daisies) == 0:
+            self.local_temperature = (1 - self.albedo) * self.model.solar_luminosity
