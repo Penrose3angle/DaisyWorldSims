4f3025c5716f71fd7295c0036d41805745efd692
Penrose3angle <fowl_ireland@hotmail.com>
2022-11-12 03:20:04 +0700

    add temperature diffusion function

--- agents.py
+++ agents.py
@@ -19,10 +19,7 @@ class Daisy(mesa.Agent):
 
     def change_local_temperature(self):
         """
-        Change the local temperature
-        every patch will calculate the temperature at that spot based on
-        (1) the energy absorbed by the daisy at that patch and
-        (2) the diffusion of 50% of the temperature value at that patch between its neighbors
+
         :param grid_content:
         :return:
         """
@@ -33,15 +30,17 @@ class Daisy(mesa.Agent):
 
     def spawn(self):
         """
-        Random chance of spawning to neighbouring grid based on the local(?) world(?) temperature
+        Random chance of spawning to neighbouring grid based on the *local* temperature
         :param grid_content:
         :return:
         """
-        # grid_content = self.model.grid.get_cell_list_contents(self.pos)
-        # grass = [a for a in grid_content if isinstance(a, GrassPatch)][0]
-        # local_temp = grass.local_temperature
+        grid_content = self.model.grid.get_cell_list_contents(self.pos)
+        grass = [a for a in grid_content if isinstance(a, GrassPatch)][0]
         seed_threshold = (
-                (0.1457 * self.model.world_temperature) - (0.0032 * (self.model.world_temperature ** 2)) - 0.6443)
+                (0.1457 * grass.local_temperature)
+                - (0.0032 * (grass.local_temperature ** 2))
+                - 0.6443)
+
         seed = random.random()
         if seed < seed_threshold:
             neighbourhood = self.model.grid.get_neighborhood(self.pos,
@@ -102,8 +101,36 @@ class GrassPatch(mesa.Agent):
             self.set_local_temperature(absorbed_luminosity)
 
     def set_local_temperature(self, absorbed_luminosity):
+        """
+        Change the local temperature
+        every patch will calculate the temperature at that spot based on
+        (1) the energy absorbed by the daisy (or if there is no daisy, the surface energy) at that patch and
+        (2) the diffusion of 50% of the temperature value at that patch between its neighbors
+        :param absorbed_luminosity:
+        :return:
+        """
         if absorbed_luminosity > 0:
             local_heating = 72 * math.log(absorbed_luminosity) + 80
         else:
             local_heating = 80
         self.local_temperature = float((self.local_temperature + local_heating) / 2)
+        self._diffuse()
+
+    def _diffuse(self, degree=0.5):
+        """
+        Each patch
+        - keep {degree} of own local temperature,
+        - give away {1-degree} of own local temperature to 8 neighbours equally
+        - get back {degree}/8.0 * each neighbour's local temperature
+        """
+        neighbourhood = self.model.grid.get_neighborhood(self.pos,
+                                                         moore=True,
+                                                         include_center=False)
+        neighbourhood_patches = []
+        for pos in neighbourhood:
+            grid_content = self.model.grid.get_cell_list_contents(pos)
+            grass = [a for a in grid_content if isinstance(a, GrassPatch)][0]
+            neighbourhood_patches.append(grass)
+
+        temps = [n.local_temperature for n in neighbourhood_patches]
+        self.local_temperature = (degree * self.local_temperature) + (1-degree) * float(sum(temps)/len(temps))
